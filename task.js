11909abdfd2c7d19f55ecff7f0c0e37807b90ca8
Anton_viarenich <Anton_viarenich@epam.com>
2018-11-27 12:59:07 +0300

    refactored

--- task.js
+++ task.js
@@ -1,46 +1,53 @@
 const fs = require('fs');
 
-const path = './note.json';
+const pathFromArgs = require('yargs').argv.path;
+const path = pathFromArgs || './notes.json';
 
-// eslint-disable-next-line no-unused-vars
 const yargs = require('yargs')
   .usage('$0 <cmd> [args]')
+  .option('path', {
+    alias: 'p',
+    describe: 'path to .json file with notes'
+  })
   .command(
     'add [title] [body]',
     'Add new unique note with the [title] and the [body]',
     {},
     function add (argv) {
-      let file = readFile();
-      checkTitle(file, argv.title);
-      file = addNote(file, argv.title, argv.body);
-      writeToFile(file);
+      if (!argv.title || !argv.body) { throw new Error('[Title] or [body] cannot be empty'); }
+      let jsonObject = readFile();
+      checkTitleExistence(jsonObject, argv.title);
+      jsonObject.push({ title: argv.title, body: argv.body });
+      writeToFile(jsonObject);
       console.clear();
-      console.log('Note added successfully.');
+      console.log('Logger:\tNote added successfully.');
     })
   .command(
     'list',
     'List all notes',
     {},
     function list () {
-      let file = readFile();
-      showAllNotes(file);
+      const jsonObject = readFile();
+      showAllNotes(jsonObject);
     })
   .command(
     'read [title]',
     'Read one note by it\'s [title]',
     {},
     function read (argv) {
-      const file = readFile();
-      showNote(file, argv.title);
+      if (!argv.title) { throw new Error('[Title] cannot be empty'); }
+      const jsonObject = readFile();
+      showNote(jsonObject, argv.title);
     })
   .command(
     'remove [title]',
-    'Remove one note by it\'s title [title]',
+    'Remove one note by it\'s [title]',
     {},
     function remove (argv) {
-      const file = readFile();
-      const modifiedFile = removeNote(file, argv.title);
-      writeToFile(modifiedFile);
+      if (!argv.title) { throw new Error('[Title] cannot be empty'); }
+      let jsonObject = readFile();
+      jsonObject = removeNote(jsonObject, argv.title);
+      writeToFile(jsonObject);
       console.clear();
       console.log('Note deleted successfully.');
     })
@@ -49,25 +56,15 @@ const yargs = require('yargs')
   .argv;
 
 function readFile () {
-  if (!fs.existsSync(path)) {
-    throw new Error(`File "${path}" not found`);
-  }
+  if (!fs.existsSync(path)) { throw new Error(`File "${path}" not found`); }
   return require(path);
-}
-
-function addNote (object, title, body) {
-  if (!title || !body) {
-    throw new Error('[Title] or [body] cannot be empty');
-  }
-  object.push({ title: title, body: body });
-  return object;
 }
 
 function writeToFile (object) {
   fs.writeFileSync(path, JSON.stringify(object, null, ' '));
 }
 
-function checkTitle (object, title) {
+function checkTitleExistence (object, title) {
   object.forEach(element => {
     if (element.title === title) throw new Error(`Note with the "${title}" title already exists`);
   });
@@ -87,19 +84,19 @@ function showAllNotes (object) {
 
 function showNote (object, title) {
   console.clear();
-  console.log('Note:\n- - -');
-  object.forEach(element => {
+  const note = object.filter(element => {
     if (element.title.toLowerCase() === title.toLowerCase()) {
+      console.log('Note:');
       Object.keys(element).forEach(key => {
         console.log(key + ': ' + element[key]);
       });
+      return element;
     }
   });
-  console.log('- - -');
+  if (!note.length) { throw new Error(`Note with the title "${title}" wasn't found`); }
 }
 
 function removeNote (object, title) {
-  console.clear();
   const newObject = object.filter(element => {
     if (element.title.toLowerCase() !== title.toLowerCase()) {
       return element;
